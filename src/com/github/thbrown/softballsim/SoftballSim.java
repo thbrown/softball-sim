49e02d89eb5d4d825dc11db23a1dc22c092422c6
Lauren Aberle <aberle.lauren@gmail.com>
2017-03-27 22:52:26 -0600

    Add LineupType enum to handle LineupGeneratorFactory instantiation. Fix a typo.
    Also improve some logging.

--- src/com/github/thbrown/softballsim/SoftballSim.java
+++ src/com/github/thbrown/softballsim/SoftballSim.java
@@ -1,56 +1,24 @@
 package com.github.thbrown.softballsim;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
 
 import com.github.thbrown.softballsim.lineup.BattingLineup;
-import com.github.thbrown.softballsim.lineupgen.AlternatingBattingLineupGenerator;
 import com.github.thbrown.softballsim.lineupgen.LineupGenerator;
-import com.github.thbrown.softballsim.lineupgen.OrdinaryBattingLineupGenerator;
+import com.github.thbrown.softballsim.lineupgen.LineupType;
 
 public class SoftballSim {
   // Config -- TODO convert these to flags w/ defaults
   public static final int INNINGS_PER_GAME = 6;
   public static final int GAMES_TO_SIMULATE = 10000;
   public static final boolean VERBOSE = false;
-  public static final int NAME_PADDING = 24; // Just for formatting verbose
-                                             // output
-  public static final String STATS_FILE_PATH = System.getProperty("user.dir") + File.separator
-      + "stats";
-
-  // Register all new batting lineups here
-  public static final Map<Integer, LineupGeneratorFactory> LINEUP_TYPES = new HashMap<>();
-  static {
-    LINEUP_TYPES.put(0, () -> new OrdinaryBattingLineupGenerator());
-    LINEUP_TYPES.put(1, () -> new AlternatingBattingLineupGenerator());
-  }
+  public static final int NAME_PADDING = 24; // Just for formatting verbose output
+  public static final String STATS_FILE_PATH =
+      System.getProperty("user.dir") + File.separator + "stats";
 
   public static void main(String[] args) {
 
-    // User input validation stuff
-    if (args.length == 0) {
-      System.out.println("usage: java " + getApplicationName() + " <lineupGeneratorNumber>");
-      System.out.println("\tExpecting input files in " + STATS_FILE_PATH);
-      printAvailableLineupTypes();
-      System.exit(0);
-    }
-
-    Integer selection = null;
-    try {
-      selection = Integer.parseInt(args[0]);
-    } catch (NumberFormatException e) {
-      System.out.println("Selection must be numerical. Was " + args[0]);
-      System.exit(1);
-    }
-
-    LineupGeneratorFactory generatorFactory = LINEUP_TYPES.get(selection);
-    if (generatorFactory == null) {
-      System.out.println("Invalid lineup type selected. Was " + selection);
-      printAvailableLineupTypes();
-      System.exit(1);
-    }
-    LineupGenerator generator = generatorFactory.getLineupGenerator();
+    validateArgs(args);
+    LineupGenerator generator = getLineupGenerator(args[0]);
     generator.readInDataFromFile(STATS_FILE_PATH);
 
     // More fun simulation stuff
@@ -63,7 +31,7 @@ public class SoftballSim {
     System.out.println("*********************************************************************");
 
     BattingLineup lineup;
-    while ((lineup = generator.getNextLienup()) != null) {
+    while ((lineup = generator.getNextLineup()) != null) {
 
       Simulation s = new Simulation(lineup);
       double result = s.run(GAMES_TO_SIMULATE);
@@ -80,11 +48,33 @@ public class SoftballSim {
     System.out.println("Best lineup mean runs scored: " + bestResult);
   }
 
+  private static LineupGenerator getLineupGenerator(String lineupTypeString) {
+    LineupType lineupType = null;
+    try {
+      lineupType = LineupType.valueOf(lineupTypeString.toUpperCase());
+    } catch (IllegalArgumentException e) {
+      System.out.println(String.format("Invalid LineupType. Was \"%s\".", lineupTypeString));
+      printAvailableLineupTypes();
+      System.exit(1);
+    }
+
+    LineupGenerator generator = lineupType.getLineupGenerator();
+    return generator;
+  }
+
+  private static void validateArgs(String[] args) {
+    if (args.length == 0) {
+      System.out.println("Usage: java " + getApplicationName() + " <LineupType>");
+      System.out.println("\tExpecting input files in " + STATS_FILE_PATH);
+      printAvailableLineupTypes();
+      System.exit(0);
+    }
+  }
+
   public static void printAvailableLineupTypes() {
     System.out.println("\tAvailable lineup generators:");
-    for (Integer i : LINEUP_TYPES.keySet()) {
-      System.out.println("\t\t" + i + ") "
-          + LINEUP_TYPES.get(i).getLineupGenerator().getClass().getSimpleName());
+    for (LineupType lineupType : LineupType.values()) {
+      System.out.println("\t\t" + lineupType);
     }
   }
 
--- src/com/github/thbrown/softballsim/lineupgen/LineupGenerator.java
+++ src/com/github/thbrown/softballsim/lineupgen/LineupGenerator.java
@@ -9,11 +9,11 @@ public interface LineupGenerator {
    * get the next possible lineup. This method should return null when there are
    * no more lineups to simulate.
    */
-  public BattingLineup getNextLienup();
+  public BattingLineup getNextLineup();
 
   /**
    * Pull data from all files in the immediate supplied directory. This is
-   * called before any calls to {@link #getNextLienup()}
+   * called before any calls to {@link #getNextLineup()}
    */
   void readInDataFromFile(String statsPath);
 }
